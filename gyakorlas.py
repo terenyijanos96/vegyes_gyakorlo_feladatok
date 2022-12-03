1ee6e01d077d8c8cf313297d918cfdd240274231
terenyijanos96 <terenyi.janos96@gmail.com>
2022-12-03 11:22:07 +0100

    első commit

---
+++ gyakorlas.py
@@ -0,0 +1,40 @@
+import random
+
+sorozat = [-3, 5, 11, -2, 4]
+
+def separator(sep=" "):
+    s = ""
+    i = 0
+    while i < len(sorozat):
+        s += str(sorozat[i])
+
+        if i < (len(sorozat) - 1):
+           s += f"{sep} "
+        i+=1
+    print(s)
+
+def random_szam_hozzado_az_elso_szamhoz():
+    rand = int(random.random() * 8) + 5
+    sorozat[0] += rand
+
+def utolso_szamjegy_helyett_ketjegyu_harommal_oszthato():
+    szam = int(input("Kérek egy kétjegyű, hárommal osztható számot! (pl: 33): "))
+    while not (9 < szam < 100 and szam % 3 == 0):
+        szam = int(input("Nem jó! Egy kétjegyű, hárommal osztható számot kérek!: "))
+
+    sorozat[len(sorozat)-1] = szam
+
+def elso_ketjegyu():
+    i = 0
+    while not(9 < sorozat[i] < 100):
+        i+=1
+
+    print(f"Első kétjegyű szám indexe: {i}, értéke: {sorozat[i]}")
+
+def prime(szam):
+    i = 1
+    while i < szam**0.5 and szam % i != 0:
+        i+=1
+    print(i)
+
+
---
+++ main.py
@@ -0,0 +1,15 @@
+
+from gyakorlas import *
+
+
+"""separator("#")
+
+random_szam_hozzado_az_elso_szamhoz()
+
+utolso_szamjegy_helyett_ketjegyu_harommal_oszthato()
+
+separator()
+
+elso_ketjegyu()"""
+
+prime(22)
