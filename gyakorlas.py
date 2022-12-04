d910ebe407ef877fcb465736b917850645157f40
Terényi János <terenyi.janos96@gmail.com>
2022-12-04 21:27:21 +0100

    prime függvény megoldása math.sqrt-al

--- gyakorlas.py
+++ gyakorlas.py
@@ -3,6 +3,7 @@ import math
 
 sorozat = [-3, 5, 11, -2, 4]
 
+
 def separator(sep=" "):
     s = ""
     i = 0
@@ -10,39 +11,43 @@ def separator(sep=" "):
         s += str(sorozat[i])
 
         if i < (len(sorozat) - 1):
-           s += f"{sep} "
-        i+=1
+            s += f"{sep} "
+        i += 1
     print(s)
+
 
 def random_szam_hozzado_az_elso_szamhoz():
     rand = int(random.random() * 8) + 5
     sorozat[0] += rand
     print(f"Első karakterhez hozzáadtunk {rand}-t. Így változott a sorozat: ", sorozat)
 
+
 def utolso_szamjegy_helyett_ketjegyu_harommal_oszthato():
     szam = int(input("Kérek egy kétjegyű, hárommal osztható számot! (pl: 33): "))
     while not (9 < szam < 100 and szam % 3 == 0):
         szam = int(input("Nem jó! Egy kétjegyű, hárommal osztható számot kérek!: "))
 
-    sorozat[len(sorozat)-1] = szam
+    sorozat[len(sorozat) - 1] = szam
     print(f"Az utolsó elemet lecseréltük a megadott számra: ", sorozat)
+
 
 def elso_ketjegyu():
     i = 0
-    while not(9 < sorozat[i] < 100):
-        i+=1
+    while not (9 < sorozat[i] < 100):
+        i += 1
 
     print(f"Első kétjegyű szám indexe: {i}, értéke: {sorozat[i]}")
 
+
 def prime(szam):
-    if szam < 2:
-        return False
+    if (szam > 2 and szam % 2 == 0) or szam < 2:  # ezzel gyorsan ki lehet szűrni a 2-nél nagyobb páros számokat,
+        return False  # illetve a kettőnél (a legkisebb primszámnál) kisebb számokat
 
     i = 2
-    while i < szam:
+    while i < math.sqrt(szam):
         if szam % i == 0:
             return False
-        i+=1
+        i += 1
     return True
 
 
@@ -51,7 +56,7 @@ def hany_prime():
     c = 0
     while i < len(sorozat):
         if prime(sorozat[i]):
-            c+=1
-        i+=1
+            c += 1
+        i += 1
 
     print("ennyi prímszám van a sorozatban:", c)
